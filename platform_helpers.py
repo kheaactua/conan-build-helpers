934b278157a10f05dcc99ad411ece3e121de9e2f
Matthew Russell <mrussell@neptec.com>
2018-03-20 12:19:04 -0400

    Updated to appendPkgConfigPath

--- platform_helpers.py
+++ platform_helpers.py
@@ -29,12 +29,16 @@ def splitPaths(paths):
 
     return paths.split(';' if 'Windows' == platform.system() else ':')
 
-def appendPkgConfigPath(paths, conan_obj):
+def appendPkgConfigPath(paths, env_obj):
     """
     Append to the a conan's virtual environments pkg config path by taking the
     current environment's pkg-config path into consideration
 
     The result is that paths will be prepended to the current pkg-config path.
+
+    @param paths Either a string of paths or list of paths
+    @param env_obj Either a conan.models.EnvInfo (usually used when called from
+                   pkg_config), or a dict (usually when called from build)
     """
 
     # Make sure we keep the default path
@@ -44,6 +48,23 @@ def appendPkgConfigPath(paths, conan_obj):
         else:
             paths = splitPaths(paths) + splitPaths(os.environ['PKG_CONFIG_PATH'])
 
-    conan_obj.env_info.PKG_CONFIG_PATH = paths
+    # Remove duplicates
+    paths = remove_duplicates_keep_order(paths)
+
+    if isinstance(env_obj, dict):
+        env_obj['PKG_CONFIG_PATH'] = paths
+    elif re.search('conans.model.env_info.EnvInfo', str(type(env_obj))):
+        # Terrible way to detect type, but I can't import conans here, so I
+        # can't check isinstance
+        env_obj.PKG_CONFIG_PATH = paths
+    else:
+        raise ValueError('Unsure of how to use provided environment object.')
+
+
+def remove_duplicates_keep_order(seq):
+    """ Source: https://stackoverflow.com/a/480227/1861346 """
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
 
 # vim: ts=4 sw=4 expandtab ffs=unix ft=python fileencoding=latin1 foldmethod=marker :
