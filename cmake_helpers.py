f2d1d4d0829f1e5f675436520621eab0ebf5705b
Matthew Russell <mrussell@neptec.com>
2018-11-01 11:52:42 -0400

    added an additional commands parameter, and protected the file name (in windows)

--- cmake_helpers.py
+++ cmake_helpers.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import os, shutil
 
 
-def wrapCMakeFile(source_folder, output_func=print, custom_cmakefile=None):
+def wrapCMakeFile(source_folder, output_func=print, custom_cmakefile=None, additional_cmds=None):
     """
     Given a source directory, this file wraps the parent CMakeLists.txt in
     another CMakeFile.  The purpose is to be able to append to certain values
@@ -17,6 +17,9 @@ def wrapCMakeFile(source_folder, output_func=print, custom_cmakefile=None):
     @param custom_cmakefile Path to custom CMakeLists.txt file if one exists
                          (say conan exports one), without this, a default wrapper CMakeLists.txt
                          file is added.
+    @param additional_cmds Additional text to be added to the top CMakeLists
+                         file, this can be used to inject basically anything.
+                         (add_definitions, etc.)
     """
 
     orig_cmake_file    = os.path.join(source_folder, 'CMakeLists.txt')
@@ -31,22 +34,35 @@ def wrapCMakeFile(source_folder, output_func=print, custom_cmakefile=None):
 
     shutil.move(src=orig_cmake_file, dst=wrapped_cmake_file)
 
+    from platform_helpers import adjustPath
+
     new_cmake_contents = '''
 PROJECT(conan_cmake_wrapper)
 
 cmake_minimum_required(VERSION 2.8)
-if (NOT "${ADDITIONAL_CXX_FLAGS}" STREQUAL "")
+set(ADDITIONAL_CXX_FLAGS "" CACHE STRING "CXX flags to append to default CMAKE_CXX_FLAGS")
+if(NOT "${ADDITIONAL_CXX_FLAGS}" STREQUAL "")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ADDITIONAL_CXX_FLAGS}")
     message(STATUS "Appending CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}")
 endif()
 
-if (NOT "${ADDITIONAL_C_FLAGS}" STREQUAL "")
+set(ADDITIONAL_C_FLAGS "" CACHE STRING "C flags to append to default CMAKE_C_FLAGS")
+if(NOT "${ADDITIONAL_C_FLAGS}" STREQUAL "")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ADDITIONAL_C_FLAGS}")
     message(STATUS "Appending CMAKE_C_FLAGS: ${CMAKE_C_FLAGS}")
 endif()
 
+set(ADDITIONAL_DEFINITIONS "" CACHE STRING "Definitions to add with add_definitions")
+if(NOT "${ADDITIONAL_DEFINITIONS}" STREQUAL "")
+    add_definitions(${ADDITIONAL_DEFINITIONS})
+    message(STATUS "Defining: ${ADDITIONAL_DEFINITIONS}")
+endif()
+
 include("%s")
-'''%(wrapped_cmake_file)
+'''%(adjustPath(wrapped_cmake_file))
+
+    if additional_cmds is not None:
+        new_cmake_contents += "\n%s"%additional_cmds
 
     output_func('Writting wrapper CMakeLists.txt file to %s which will include %s'%(orig_cmake_file, wrapped_cmake_file))
     with open(orig_cmake_file, 'w') as f: f.write(new_cmake_contents)
